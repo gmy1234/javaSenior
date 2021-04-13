1ea62740a98ddd43f690500153596d0fc404f368
gmy <1508594767@qq.com>
2021-04-13 23:47:17 +0800

    集合

--- Day06/src/Collection/collectionTest2.java
+++ Day06/src/Collection/collectionTest2.java
@@ -23,6 +23,7 @@ public class collectionTest2 {
         //遍历集合的元素
         while ( iterator.hasNext()){
             System.out.println(iterator.next());
+
         }
         //hasNext()判断集合的下一个地址还有元素
         //next() 1.指针向下移动一个地址，2将下移的元素返回
